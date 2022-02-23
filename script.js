8dffd6b9128b4d4cabaf2fcf0cae489295b12de5
darzwaitz <darzwaitz@yahoo.com>
2022-02-23 20:45:06 +0000

    added navigator geolocation functionality

--- script.js
+++ script.js
@@ -10,3 +10,17 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      //   console.log(position);
+      const { latitude } = position.coords; // destructured, as same name of variable exists on coords
+      //   const { longitude } = position.coords;
+      const { longitude } = position.coords;
+      //   console.log(`lat: ${latitude}, long: ${longitude}`);
+    },
+    function () {
+      alert('Could not grab your position');
+    }
+  );
